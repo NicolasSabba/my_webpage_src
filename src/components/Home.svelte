5106cd92d78acfd8132ee88e8b64c997c3d4ece1
Nicolas <nicolas.sabbatini.95@gmail.com>
2020-05-29 01:27:47 -0300

    Finish home

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -6,7 +6,7 @@
     const interval = setInterval(() => {
         start_name = true;
         clearInterval(interval);
-    }, 3000);
+    }, 1000);
 
     onDestroy(() => clearInterval(interval));
 
@@ -22,6 +22,28 @@
             'Is this thing on?',
             'This is in fact not Minecraft.',
             '01001110 01100101 01110010 01100100',
+            '😍💓💕💖💗💘💝💞',
+            'You are a handsome little bastard',
+            '(づ￣ ³￣)づ I love you',
+            'Yeet (╯°□°）╯︵ ┻━┻',
+            '#TeamtTrees',
+            'Error...',
+            'No dessert for you',
+            'Do you have McDonald´s money?',
+            'You are a curious person... I like you',
+            'SHAKESPEARE QUOTE OF THE DAY: An SSL error has occurred and a secure connection to the server cannot be made. "An SSL error has occured and a secure connection to the server , cannot be made"',
+            'Send money',
+            'You now have to send money',
+            '🎶 That it´s a thriller, thriller night 🎶',
+            '(☞ﾟヮﾟ)☞ Yaaaas',
+            'ฅ^•ﻌ•^ฅ Meowwwww',
+            '(•̀ᴗ•́)و ̑̑ This works',
+            '(ง •̀_•́)ง Say that to my face',
+            '"Spaghettiiii" ლ(ಠ_ಠლ)',
+            '( ˘ ³˘)♥💕',
+            '(~‾▿‾)~ It is my favourite song',
+            '┬┴┬┴┤ ͜ʖ ͡°) ├┬┴┬┴ I know what are you doing',
+            'HaVe YOu eVeR hAd a DrEAm?',
         ];
         random_phrase = rp[Math.floor(Math.random() * rp.length)];
         start_random = true;
@@ -36,7 +58,8 @@
 
     div.content div.terminal{
         margin: auto;
-        margin-top: 10px; 
+        margin-top: 10px;
+        min-width: 1100px; 
         width: 98%;
         height: 400px;
         background-color: #333;
@@ -64,7 +87,7 @@
         text-align: left;
         float: left;
         margin-left: 0px;
-        animation: blink-caret 0.75s step-end 10;
+        animation: blink-caret 0.75s step-end 7;
     }
 
     @keyframes blink-caret {
@@ -94,9 +117,43 @@
         color: var(--text);
         font-size: 20px;
         position: absolute;
+        max-width: 60%;
         top: 223px;
         margin-left: 156px;
         opacity: 0.5;
+    }
+
+    div.content div.info {
+        columns: 2;
+        margin: auto;
+        margin-top: 15px;
+        width: 800px;
+    }
+
+    div.content div.info div.img {
+        display: inline-block;
+        width: 350px;
+    }
+
+    div.content div.info div.img img {
+        border-radius: 50%;
+        width: 350px;
+    }
+
+    div.content div.info div.text {
+        padding-top: 75px;
+        font-size: 20px; 
+    }
+
+    div.content div.info div.text p {
+        color: var(--text);
+        text-align: left;
+        margin: auto;
+    }
+
+    div.content div.info div.text a {
+        color: var(--text);
+        text-decoration: underline;
     }
 
 </style>
@@ -122,6 +179,16 @@
         {/if} 
     </div>
     <div class="info">
-        <p></p>
+        <div class="img">
+            <img class="profile" src="https://pbs.twimg.com/profile_images/1244438560918384640/7yPACbDA_400x400.jpg" alt="ProfilePicture">
+        </div>
+        <div class="text">
+            <p> Hi! I'm Nicolás Sabbatini.</p>
+            <p> I am a very creative and passionate person.</p>
+            <p> I like art and programming and I love matte colors.</p>
+            <p> I am currently working at <a href="https://santexgroup.com/" target="_blank">Santex</a>,
+                studying at <a href="https://www.famaf.unc.edu.ar/" target="_blank">Famaf</a>, and trying to fulfill my dreams.</p>
+            <p> I really hope you like what you´ll find here.</p>
+        </div>
     </div>
 </div>
