4662c13f3f6020b7f540724f688f9b3239321a95
Nicolas <nicolas.sabbatini.95@gmail.com>
2020-05-29 00:38:33 -0300

    Icons

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -37,43 +37,11 @@
     nav a.link.inactive {
         color: var(--navbar-text-shadow);
     }
-
-    nav a .home {
-        padding: 0px;
-        margin: 18px;
-        stroke: var(--text);
-        float: left;
-        height: 29px;
-        width: 29px;
-        fill: none;
-        stroke-linecap: round;
-        stroke-linejoin: round;
-        stroke-width: 50px;
-    }
-
-    nav a .home.active {
-            stroke: var(--text);
-    }
-
-    nav a .home:hover.active {
-        stroke: var(--navbar-text-shadow);
-        cursor: pointer;
-    }
-
-    nav a .home.inactive {
-        stroke: var(--navbar-text-shadow);
-    }
-
 </style>
 
 <nav>
-    <a href="/#">
-        <svg width='512' height='512' viewBox='0 0 512 512' class='home {active === "/" || active === "/#" ? "inactive" : "active"}' on:click='{() => change_active("/#")}'>
-            <title>ionicons-v5-i</title>
-            <path d='M80,212V448a16,16,0,0,0,16,16h96V328a24,24,0,0,1,24-24h80a24,24,0,0,1,24,24V464h96a16,16,0,0,0,16-16V212'/>
-            <path d='M480,256,266.89,52c-5-5.28-16.69-5.34-21.78,0L32,256'/>
-            <polyline points='400 179 400 64 352 64 352 133'/>
-        </svg>
+    <a href="/#" class='link {active === "/" || active === "/#" ? "inactive" : "active"}' style="float: left" on:click='{() => change_active("/#")}'>
+        <i class="fas fa-home"></i>
     </a>
     <!--
     <a href="#/blog" class='link {active === "#/blog" ? "inactive" : "active"}' on:click='{() => change_active("blog")}'>Blog</a>
